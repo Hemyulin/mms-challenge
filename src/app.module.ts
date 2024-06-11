91f4946cf7f0a593dc22b570a1487ccc61521b19
Hemyulin <daniel.budiansky@protonmail.com>
2024-06-11 12:24:36 +0200

    imports fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { OrderService } from './order/order.service';
 import { OrderResolver } from './order/order.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
