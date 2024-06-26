53c57c6700a8acb94227cd09eef9280b6fef120a
Hemyulin <daniel.budiansky@protonmail.com>
2024-06-26 13:09:53 +0200

    date scalar removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrderModule } from './order/order.module';
-import { DateScalar } from './graphql/date.scalar';
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
 
@@ -16,6 +15,5 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
     }),
     OrderModule,
   ],
-  providers: [DateScalar],
 })
 export class AppModule {}
