55d52043d3b54bd2156a163a576cc5a55ceab61b
Hemyulin <daniel.budiansky@protonmail.com>
2024-06-26 14:05:02 +0200

    adding employee schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrderModule } from './order/order.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
 
@@ -16,4 +17,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
     OrderModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('graphql');
+  }
+}
